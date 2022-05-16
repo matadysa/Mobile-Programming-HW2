f6544b7fff69a38dd61d10ca90bb60bd136527c4
Mahdi Khoshdell <khoshdellmahdi@gmail.com>
2022-05-16 11:09:36 +0430

    view Pager in start Activity

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();
             }
-        }, 4500);
+        }, 1500);
     }
 }
--- app/src/main/java/com/example/weatherapp/StartActivity.java
+++ app/src/main/java/com/example/weatherapp/StartActivity.java
@@ -1,10 +1,17 @@
 package com.example.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class StartActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +20,14 @@ public class StartActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
+        tabLayout = findViewById(R.id.main_tab);
+        viewPager = findViewById(R.id.view_pager);
 
+        tabLayout.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new SearchFragment(), "Search");
+        vpAdapter.addFragment(new SettingFragment(), "Setting");
+        viewPager.setAdapter(vpAdapter);
     }
 }
