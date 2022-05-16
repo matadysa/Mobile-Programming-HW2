64d266986fb28ecb8e84d1ee39aebf8d4030e963
Matadysa <matadysa@gmail.com>
2022-05-16 18:00:28 +0430

    tab selection complete

--- app/src/main/java/com/example/weatherapp/StartActivity.java
+++ app/src/main/java/com/example/weatherapp/StartActivity.java
@@ -6,10 +6,14 @@ import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class StartActivity extends AppCompatActivity {
 
     private FragmentsStateAdapter fragmentsStateAdapter;
     private ViewPager2 viewPager;
+
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +23,36 @@ public class StartActivity extends AppCompatActivity {
         fragmentsStateAdapter = new FragmentsStateAdapter(getSupportFragmentManager(), getLifecycle());
         viewPager = findViewById(R.id.container);
         setupViewPager(viewPager);
+
+        tabLayout = findViewById(R.id.main_tab);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    setViewPager(0);
+                } else {
+                    setViewPager(1);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 
     private void setupViewPager(ViewPager2 viewPager) {
         FragmentsStateAdapter adapter = new FragmentsStateAdapter(getSupportFragmentManager(), getLifecycle());
+        adapter.addFragment(new SearchFragment());
         adapter.addFragment(new DarkModePageFragment());
         viewPager.setAdapter(adapter);
+    }
+
+    public void setViewPager(int fragmentNumber) {
+        viewPager.setCurrentItem(fragmentNumber);
     }
 }
