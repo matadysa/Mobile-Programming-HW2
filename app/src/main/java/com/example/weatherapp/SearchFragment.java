f9a555fb9532526c2cc4d4a2ee6181b79a9936d0
Matadysa <matadysa@gmail.com>
2022-05-17 13:46:47 +0430

    network security config file added

--- app/src/main/java/com/example/weatherapp/SearchFragment.java
+++ app/src/main/java/com/example/weatherapp/SearchFragment.java
@@ -8,6 +8,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,8 +16,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.DecimalFormat;
 
 public class SearchFragment extends Fragment {
 
@@ -24,6 +32,14 @@ public class SearchFragment extends Fragment {
     RadioButton fiAndLRadioButton, cityNameRadioButton;
     TextInputEditText fiInputText, LInputText, cityNameInputText;
     Button searchButton;
+
+    //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+    private final String weatherApiUrl = "api.openweathermap.org/data/2.5/weather?";
+    //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+    private final String coordinateApiUrl = "https://api.openweathermap.org/data/2.5/onecall?";
+    private final String appId = "b45229916cb8cfb3ec05d4579870f195";
+
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,7 +62,29 @@ public class SearchFragment extends Fragment {
     }
 
     private void getWeatherInformation(String cityName, Double longitude, Double latitude) {
-        
+        String url = "";
+        if (cityName == null) {
+            url = coordinateApiUrl +
+                    "lat=" + df.format(latitude) +
+                    "&lon=" + df.format(longitude) +
+                    "&exclude=minutely,hourly,alerts" +
+                    "&appid=" + appId;
+        } else {
+            url = weatherApiUrl + "q=" + cityName + "&appid=" + appId;
+        }
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("response", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
 }
